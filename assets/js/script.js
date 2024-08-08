662ee06f5edc152e43fa62494b3ce7940459dcf0
Mickael <M1ck4el@protonmail.com>
2024-08-08 15:08:28 +0200

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,15 @@ document.addEventListener("DOMContentLoaded", () => {
     tableContents.classList.toggle("active");
   });
 });
+
+// -------------------
+
+// document.addEventListener("scroll", handleScroll);
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+scrollToTopBtn.addEventListener("click", scrollToTop);
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
