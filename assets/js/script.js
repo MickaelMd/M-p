4cdc4889df85991d771c1340362987ee88a7e570
Mickael <M1ck4el@protonmail.com>
2024-08-10 13:32:34 +0200

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,6 @@ function scrollToTop() {
 
 document.querySelectorAll(".clickable-image").forEach((image) => {
   image.addEventListener("click", () => {
-    // Crée l'overlay s'il n'existe pas déjà
     let overlay = document.querySelector(".fullscreen-overlay");
     if (!overlay) {
       overlay = document.createElement("div");
@@ -30,28 +29,23 @@ document.querySelectorAll(".clickable-image").forEach((image) => {
       document.body.appendChild(overlay);
     }
 
-    // Clone l'image et la transforme s'il n'y a pas déjà une image agrandie
     let imgClone = document.querySelector(".fullscreen-image");
     if (!imgClone) {
       imgClone = image.cloneNode();
       imgClone.classList.add("fullscreen-image");
       document.body.appendChild(imgClone);
     } else {
-      // Si l'image est déjà en grand, la cacher et supprimer l'overlay
       imgClone.remove();
       overlay.remove();
     }
 
-    // Affiche l'overlay et l'image
     overlay.style.display = "block";
 
-    // Ferme l'image et enlève l'overlay quand on clique dessus
     overlay.addEventListener("click", () => {
       imgClone.remove();
       overlay.remove();
     });
 
-    // Ferme l'image quand on clique directement dessus
     imgClone.addEventListener("click", () => {
       imgClone.remove();
       overlay.remove();
